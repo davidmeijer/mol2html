1b4b91d01f8290b3d4bd9deb64ad14b0c3ce7c63
davidmeijer <david-meijer@live.nl>
2021-12-01 02:14:01 +0100

    ENH: added example script for highlighting peptide bonds in structure

---
+++ highlight_peptide_bonds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Author:         David Meijer
+Description:    Generates self-contained interactive 3D molecule HTML with 
+                individual amino acids highlighted.
+Usage:          python3 highlight_amino_acids.py <input.sdf>
+Dependencies:   See dependencies folder; mol2html; rdkit
+"""
+from sys import argv
+from mol2html import mol2html
+from rdkit import Chem
+
+def main() -> None:
+    """Driver code.
+    """
+    pathToSdf = argv[1]
+    with open(pathToSdf, 'r') as fo: sdfString = fo.read()
+
+    # Mine SMILES string for amino acid substructures and color them accordingly.
+    mol = Chem.MolFromMolBlock(sdfString)
+    atomStyles = {atom.GetIdx() : {'color' : 'black'} for atom in mol.GetAtoms()}   
+    matches = mol.GetSubstructMatches(Chem.MolFromSmiles('C(=O)N'))
+    for match in matches: 
+        for atomIdx in match:
+            atomStyles[atomIdx]['color'] = 'red'
+
+    # Compose HTML.
+    htmlString = mol2html(Chem.MolToMolBlock(mol), 'daptomycin_snapshot', atomStyles)
+    with open('daptomycin.html', 'w') as foHtml: foHtml.write(htmlString)
+
+if __name__ == '__main__':
+    main()
